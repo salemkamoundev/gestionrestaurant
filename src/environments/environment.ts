aa2d3eb8f2cbf6c7a4bb1ad63143e1c331ec3cdc
Salem Kamoun <kammoun.salem@gmail.com>
2025-12-13 22:18:07 +0100

    Initial commit: Angular + Firebase + Tailwind setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBJsxQA6negkfhN2uDnpBC6uWrLBAO4p-0',
+    authDomain: 'gestionrestorant.firebaseapp.com',
+    databaseURL: 'https://gestionrestorant-default-rtdb.firebaseio.com',
+    projectId: 'gestionrestorant',
+    storageBucket: 'gestionrestorant.firebasestorage.app',
+    messagingSenderId: '205529562324',
+    appId: '1:205529562324:web:3aabf43f789ddfa0f40f41',
+    measurementId: 'G-2P07XN1Q4K'
+  }
+};
